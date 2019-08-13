bcddc2faf00083ae57a875fc2090b7e92cd21699
Farrukh <679@holbertonschool.com>
2019-08-13 22:25:36 +0000

    Remove null check

--- models/place.py
+++ models/place.py
@@ -31,6 +31,6 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
