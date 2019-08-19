8c9d33a1d8be131a276e2b0534fee245bbf55351
lroudge <lauradrc@hotmail.fr>
2019-08-19 20:30:21 +0000

    Put the required html content in the task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-MYSTR=$(dpkg -s nginx)
-if [[ $MYSTR != *"install ok installed"* ]]; then
-    apt-get -y update
-    apt-get -y install nginx
-    ufw allow 'Nginx HTTP'
-fi
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Test content of web_static" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/server_name _;/a location /hbnb_static/ {\nalias /data/web_static/current/;\nautoindex off;\n}' /etc/nginx/sites-available/default
