5df3aa1db872f6983ba76c0cdc504436591c2ab5
lroudge <lauradrc@hotmail.fr>
2019-08-20 01:32:16 +0000

    Add advanced task 5

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,73 @@
+# puppet manifest preparing a server for static content deployment
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
+}
+
+file { '/data':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'location /hbnb_static/ { alias /data/web_static/current/;}',
+  require => Package['nginx'],
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+}
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  force  => yes,
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
