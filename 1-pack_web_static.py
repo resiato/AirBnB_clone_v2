75356bc8a654acde51c068d684ae2eb4935280c6
lroudge <lauradrc@hotmail.fr>
2019-08-20 06:47:27 +0000

    Change task 1 and 2 so they return the file path

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,8 @@ def do_pack():
     """Function to compress files"""
     local("mkdir -p versions")
     result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
     if result.failed:
         return None
     return result
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,7 +17,8 @@ def do_pack():
     """Function to compress files in an archive"""
     local("mkdir -p versions")
     result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
     if result.failed:
         return None
     return result
