70498d1031d08042d8624028860269ddbfcd77e2
lroudge <lauradrc@hotmail.fr>
2019-08-29 23:56:10 +0000

    Add task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at that route"""
+    return render_template('7-states_list.html', states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run()
