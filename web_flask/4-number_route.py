401bb69944e34b7cb22fe2af9dc645adfad3ae78
lroudge <lauradrc@hotmail.fr>
2019-08-29 21:07:09 +0000

    Initial commit of project 0x04

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at that route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string at that route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Returns a string at that route"""
+    new = text.replace('_', ' ')
+    return 'C %s' % new
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """Returns a string at that route"""
+    new = text.replace('_', ' ')
+    return 'Python %s' % new
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Returns a string at that route"""
+    if type(n) == int:
+        return '%i is a number' % n
+
+if __name__ == '__main__':
+    app.run()
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at that route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string at that route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Returns a string at that route"""
+    new = text.replace('_', ' ')
+    return 'C %s' % new
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """Returns a string at that route"""
+    new = text.replace('_', ' ')
+    return 'Python %s' % new
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Returns a string at that route"""
+    if type(n) == int:
+        return '%i is a number' % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Returns a template at that route"""
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run()
